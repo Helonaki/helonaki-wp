4d284ac9d94319105c4f494dbf0bbabe08750240
Jen Glaubius <glaubius@gmail.com>
2020-02-14 10:36:55 -0500

    add services cards and padding to right side of navbar

--- front-page.php
+++ front-page.php
@@ -27,8 +27,91 @@ get_header(); ?>
   <div class="container-fluid">
     <h2>Work With The Helonaki</h2>
     <div class="row">
-      <div class="">
+      <div class="col-md-4">
+        <img src="... " alt="TBD">
+      </div>
+      <div class="col-md-8">
+        <p>GIS and data analysis are integral parts of modern archaeological and
+          cultural heritage projects. New technologies increase our understanding
+        of the past, but also require new skills.</p>
+        <p><strong>The Helonaki can help you leverage new technologies so that
+          you can focus on your research.</strong></p>
+        <p>By working with The Helonaki, you save <strong>time</strong> you
+          would otherwise spend performing rote GIS or data analysis tasks.</p>
+      </div>
+    </div>
+  </div>
+</section>
 
+<section id="services" class="dark-section">
+  <div class="container-fluid">
+    <h2>Services Offered</h2>
+    <div class="row p-4">
+      <div class="col-md-6">
+        <div class="card mb-3 services-card">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">GIS and Remote Sensing</h5>
+                <p class="card-text">Tincidunt praesent semper feugiat nibh sed pulvinar. Sed velit
+                  dignissim sodales ut eu sem integer vitae. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card mb-3 services-card">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Data Analysis and Modeling</h5>
+                <p class="card-text">Tincidunt praesent semper feugiat nibh sed pulvinar. Sed velit
+                  dignissim sodales ut eu sem integer vitae. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row p-4">
+      <div class="col-md-6">
+        <div class="card mb-3 services-card">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Web Mapping</h5>
+                <p class="card-text">Tincidunt praesent semper feugiat nibh sed pulvinar. Sed velit
+                  dignissim sodales ut eu sem integer vitae. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card mb-3 services-card">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="..." class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Website Design</h5>
+                <p class="card-text">Tincidunt praesent semper feugiat nibh sed pulvinar. Sed velit
+                  dignissim sodales ut eu sem integer vitae. </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
